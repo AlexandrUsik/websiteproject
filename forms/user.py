e1398b1f6b88460dfe36d74958f8b3e501b9a268
Alexandr <sashausik56@gmail.com>
2021-04-11 17:38:10 +0700

    base of project

---
+++ forms/user.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    surname = StringField('Фамилия пользователя', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    age = TextAreaField("Возраст")
+    position = TextAreaField("Позиция")
+    about = TextAreaField("Немного о себе")
+    adress = TextAreaField("Адрес")
+    submit = SubmitField('Войти')
